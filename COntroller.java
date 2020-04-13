af1900e613d1b50dae1ba6e1f967d4f396a9ea9f
Mayur-Vinchurkar <mayurvinchurkar@gmail.com>
2020-04-12 23:23:49 -0700

    new branch commit

---
+++ COntroller.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class COntroller {
+	
+	
+	
+	@RequestMapping("/hello")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", "Test");
+                 model.put("message", "Test1");
+		return "welcome";
+	}
+}
+
+
